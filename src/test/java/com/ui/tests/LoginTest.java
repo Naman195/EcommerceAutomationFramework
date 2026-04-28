b8f726eff28ff4e14754616ab497ec1b81162b3d
Naman195 <72151532+Naman195@users.noreply.github.com>
2026-04-28 10:36:23 +0530

    base commit setup

---
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -0,0 +1,13 @@
+package com.ui.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+	
+	public static void main(String[] args) {
+		WebDriver wd =  new ChromeDriver();
+		
+	}
+
+}
