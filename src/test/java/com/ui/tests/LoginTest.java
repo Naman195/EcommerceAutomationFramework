581ba3974c39f914da4bf57f59de851ab01f0fad
Naman195 <72151532+Naman195@users.noreply.github.com>
2026-04-30 02:42:02 +0530

    Added CSVReader Utility, JSON Utility , Added Data Providers

--- src/test/java/com/ui/tests/LoginTest.java
+++ src/test/java/com/ui/tests/LoginTest.java
@@ -3,15 +3,17 @@ package com.ui.tests;
 import static com.constants.Browser.*;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.ui.pages.HomePage;
+import com.ui.pojo.User;
 
 public class LoginTest {
 	HomePage hompePage;
 	
-	@BeforeTest(description = "load the HomePage of the website")
+	@BeforeMethod(description = "load the HomePage of the website")
 	public void setup() {
 		 hompePage = new  HomePage(CHROME);
 	}
@@ -19,13 +21,26 @@ public class LoginTest {
 	
 	
 		
-		@Test(description = "Verifies with the valid user is able to login into the application", groups = {"e3e", "sanity"})
-		public void loginTest() {
+		@Test(description = "Verifies with the valid user is able to login into the application", groups = {"e3e", "sanity"}, 
+				dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestDataProvider")
+		public void loginTest(User user) {
 			
 		
 		
 		
-		String userName = hompePage.goToLoginPage().doLoginWith("gogab23467@soppat.com", "password").getUserName();
+		String userName = hompePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName();
+		Assert.assertEquals(userName, "Naman Arora");
+		
+		}
+		
+		@Test(description = "Verifies with the valid user is able to login into the application", groups = {"e3e", "sanity"}, 
+				dataProviderClass = com.ui.dataproviders.LoginDataProvider.class, dataProvider = "LoginTestCSVDataProvider")
+		public void loginCSVTest(User user) {
+			
+		
+		
+		
+		String userName = hompePage.goToLoginPage().doLoginWith(user.getEmailAddress(), user.getPassword()).getUserName();
 		Assert.assertEquals(userName, "Naman Arora");
 		
 		}
---
+++ src/test/java/com/utility/CSVReaderUtility.java
@@ -0,0 +1,49 @@
+package com.utility;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import com.ui.pojo.User;
+
+public class CSVReaderUtility {
+
+	public static Iterator<User> readCSVFile(String filename)  {
+		
+		File csvFile = new File(System.getProperty("user.dir") + "\\testData\\" + filename);
+		FileReader fileReader = null;
+		CSVReader csvReader;
+		String[] line;
+		List<User> userList = null;
+		User userData;
+		try {
+			fileReader = new FileReader(csvFile);
+			csvReader = new  CSVReader(fileReader);
+			csvReader.readNext(); /// reading the column names
+//			data = csvReader.readNext();
+			 userList = new ArrayList<User>();
+			
+			while((line = csvReader.readNext()) != null) {
+				userData = new User(line[0], line[1]);
+				userList.add(userData);
+			}
+	
+			for(User us : userList) {
+				System.out.println(us);
+			}
+			
+		} catch (IOException | CsvValidationException e) {
+			e.printStackTrace();
+		}
+		
+		
+			return userList.iterator();	
+	}
+}
